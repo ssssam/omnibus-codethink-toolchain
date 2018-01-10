b5a43e3d677a8e96220a9a7db46f680bb452550a
Lukasz Kolodziejczyk <lukasz.m.kolodziejczyk@gmail.com>
2018-01-10 15:46:13 +0000

    Create codethink-llvm project
    Create the project files: projects/codethink-llvm.rb and software/llvm.rb.
    To pass the heath check after the llvm building, the following dependencies had to be added:
    software/ncurses.rb with patches (patches/ncurses/*), software/config_guess.rb,
    software/libtool.rb, software/patch.rb and software/zlib.rb.
    All these dependencies were taken from: https://github.com/chef/omnibus-software.

---
+++ config/software/patch.rb
@@ -0,0 +1,41 @@
+#
+# Copyright 2015 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "patch"
+
+dependency "config_guess"
+
+license "GPL-3.0"
+license_file "COPYING"
+skip_transitive_dependency_licensing true
+
+default_version "2.7"
+
+version("2.7.5") { source md5: "ed4d5674ef4543b4eb463db168886dc7" }
+version("2.7") { source md5: "1cbaa223ff4991be9fae8ec1d11fb5ab" }
+source url: "https://ftp.gnu.org/gnu/patch/patch-#{version}.tar.gz"
+relative_path "patch-#{version}"
+
+env = with_standard_compiler_flags(with_embedded_path)
+
+build do
+
+  update_config_guess(target: "build-aux")
+
+  configure "--disable-xattr", env: env
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
+end
